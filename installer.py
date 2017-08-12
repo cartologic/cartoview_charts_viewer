ee424966b9797230e882d4061ac7a65af773bfc7
hisham waleed karam <hisham.karam@cartologic.com>
2017-08-12 23:54:18 +0200

    update logo && replace marksafe with safeseq

--- installer.py
+++ installer.py
@@ -2,7 +2,7 @@ __author__ = 'kamal'
 
 info = {
     "title": "Charts Viewer",
-    "description": "Charts Viewer enables you to display charts for layers data in a Cartoview Viewer map",
+    "description": "Creating charts for numeric attributes of features in a specified  layer that fall in the visible map extent. The Charts can be configured to show the sum, average, minimum and maximum of specified field values.",
     "author": 'Cartologic',
     "home_page": 'http://cartologic.com/cartoview/apps/charts_viewer',
     "help_url": "http://cartologic.com/cartoview/apps/charts_viewer/help/",
--- urls.py
+++ urls.py
@@ -1,9 +1,4 @@
-from django.conf.urls import patterns, url
 import views
 from . import APP_NAME
 
-urlpatterns = patterns('',
-   url(r'^(?P<instance_id>\d+)/view/$', views.view_app, name='%s.view' % APP_NAME),
-   url(r'^new/$', views.new, name='%s.new' % APP_NAME),
-   url(r'^(?P<instance_id>\d+)/edit/$', views.edit, name='%s.edit' % APP_NAME),
-)
+urlpatterns = views.charts_viewer.get_url_patterns()
